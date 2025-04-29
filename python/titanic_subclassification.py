b770e70eebb52fab42788f6ab779303a2b68d726
Xiaotong Lu <30410684+Maggie1216@users.noreply.github.com>
2025-04-28 22:02:45 -0400

    correct on treatment effect calculation

--- python/titanic_subclassification.py
+++ python/titanic_subclassification.py
@@ -1,47 +1,25 @@
-import numpy as np 
-import pandas as pd 
-import statsmodels.api as sm 
-import statsmodels.formula.api as smf 
-from itertools import combinations 
-import plotnine as p
+# number of obs 
+obs_u = titanic.loc[titanic.d == 0].shape[0] # for control group (non first class)
+obs_t = titanic.loc[titanic.d == 1].shape[0] # for treatment group (first class)
+obs = titanic.shape[0]
 
-# read data
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-def read_data(file): 
-	return pd.read_stata("https://github.com/scunning1975/mixtape/raw/master/" + file)
-
-
-
-
-## Weighted Average Treatment Effect 
-titanic = read_data("titanic.dta")
-
-titanic['d'] = 0
-titanic.loc[titanic['class']=='1st class', 'd'] = 1
-
-titanic['sex_d'] = 0
-titanic.loc[titanic['sex']=='man', 'sex_d'] = 1
-
-titanic['age_d'] = 0
-titanic.loc[titanic['age']=='adults', 'age_d'] = 1
-
-titanic['survived_d'] = 0
-titanic.loc[titanic['survived']=='yes', 'survived_d'] = 1
-
-titanic['s'] = 0 
-titanic.loc[(titanic.sex_d == 0) & (titanic.age_d==1), 's'] = 1
-titanic.loc[(titanic.sex_d == 0) & (titanic.age_d==0), 's'] = 2
-titanic.loc[(titanic.sex_d == 1) & (titanic.age_d==1), 's'] = 3
-titanic.loc[(titanic.sex_d == 1) & (titanic.age_d==0), 's'] = 4
-
-obs = titanic.loc[titanic.d == 0].shape[0]
-
-def weighted_avg_effect(df):
+def weighted_avg_effect(df, effect = 'ate'):
+    
     diff = df[df.d==1].survived_d.mean() - df[df.d==0].survived_d.mean()
-    weight = df[df.d==0].shape[0]/obs
+    
+    if effect == 'ate':
+        weight = df.shape[0]/obs
+    elif effect == 'att':
+        weight = df[df.d==1].shape[0]/obs_t
+    elif effect == 'atu':
+        weight = df[df.d==0].shape[0]/obs_u
+        
     return diff*weight
 
-wate = titanic.groupby('s').apply(weighted_avg_effect).sum()
+wate = titanic.groupby('s').apply(lambda x: weighted_avg_effect(x, 'ate')).sum()
+watt = titanic.groupby('s').apply(lambda x: weighted_avg_effect(x, 'att')).sum()
+watu = titanic.groupby('s').apply(lambda x: weighted_avg_effect(x, 'atu')).sum()
 
 print("The weigthted average treatment effect estimate is {:.2%}".format(wate))
+print("The weigthted average treatment effect on the treated estimate is {:.2%}".format(watt))
+print("The weigthted average treatment effect on the untreated estimate is {:.2%}".format(watu))
